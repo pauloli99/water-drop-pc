92aaeba7bd71c2639d51cd377efbbf6a493486cc
pauloli <616752336@qq.com>
2023-04-20 19:23:20 +0800

    feat: init

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  extends: [
+    require.resolve("eslint-config-airbnb"),
+    require.resolve("eslint-config-airbnb/hooks"),
+    require.resolve("eslint-config-airbnb-typescript"),
+  ],
+  rules: {
+    "react/function-component-definition": 0,
+    "react/react-in-jsx-scope": 0,
+    "import/prefer-default-export": 0,
+    "react-hooks/exhaustive-deps": 1,
+    "react/button-has-type": 0,
+    "no-extraneous-dependencies": 0,
+    "no-unused-vars": 0,
+    "@typescript-eslint/no-unused-vars": 1,
+    "jsx-a11y/control-has-associated-label": 0,
+    "import/no-extraneous-dependencies": 0
+  },
+  parser: require.resolve("@typescript-eslint/parser"),
+  parserOptions: {
+    project: require.resolve("./tsconfig.json"),
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
